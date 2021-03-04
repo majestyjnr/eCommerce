e3108e3f33f65e187c15ebf52a35af812015eaec
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-04 00:50:45 -0800

    commit updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,5 +22,5 @@ function App() {
     </Router>
   );
 }
-
+9
 export default App;
--- frontend/src/components/account/Login.jsx
+++ frontend/src/components/account/Login.jsx
@@ -18,7 +18,83 @@ import '../css/venobox.css'
 const login = () => {
     return (
         <div>
-            
+            <div className="breadcrumb-area">
+                <div className="container">
+                    <div className="breadcrumb-content">
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li className="active">Login Register</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="page-section mb-60">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30">
+                            <form action="#" >
+                                <div className="login-form">
+                                    <h4 className="login-title">Login</h4>
+                                    <div className="row">
+                                        <div className="col-md-12 col-12 mb-20">
+                                            <label>Email Address*</label>
+                                            <input className="mb-0" type="email" placeholder="Email Address"/>
+                                        </div>
+                                        <div className="col-12 mb-20">
+                                            <label>Password</label>
+                                            <input className="mb-0" type="password" placeholder="Password"/>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="check-box d-inline-block ml-0 ml-md-2 mt-10">
+                                                <input type="checkbox" id="remember_me"/>
+                                                <label for="remember_me">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-4 mt-10 mb-20 text-left text-md-right">
+                                            <a href="#"> Forgotten pasward?</a>
+                                        </div>
+                                        <div className="col-md-12">
+                                            <button className="register-button mt-0">Login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="col-sm-12 col-md-12 col-lg-6 col-xs-12">
+                            <form action="#">
+                                <div className="login-form">
+                                    <h4 className="login-title">Register</h4>
+                                    <div className="row">
+                                        <div className="col-md-6 col-12 mb-20">
+                                            <label>First Name</label>
+                                            <input className="mb-0" type="text" placeholder="First Name"/>
+                                        </div>
+                                        <div className="col-md-6 col-12 mb-20">
+                                            <label>Last Name</label>
+                                            <input className="mb-0" type="text" placeholder="Last Name"/>
+                                        </div>
+                                        <div className="col-md-12 mb-20">
+                                            <label>Email Address*</label>
+                                            <input className="mb-0" type="email" placeholder="Email Address"/>
+                                        </div>
+                                        <div className="col-md-6 mb-20">
+                                            <label>Password</label>
+                                            <input className="mb-0" type="password" placeholder="Password"/>
+                                        </div>
+                                        <div className="col-md-6 mb-20">
+                                            <label>Confirm Password</label>
+                                            <input className="mb-0" type="password" placeholder="Confirm Password"/>
+                                        </div>
+                                        <div className="col-12">
+                                            <button className="register-button mt-0">Register</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
