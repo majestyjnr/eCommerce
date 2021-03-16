04cbb18e9ac5c19923d4fd673c0efe03b5df7901
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-16 06:01:34 -0700

    Commit Updates

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = new express();
 
+const db = require('./config/db_config')
+
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
+    if(!error){
+        console.log('MongoDb connected...')
+    }else{
+        console.log('MongoDb failed to connect.')
+    }
+})
+
 app.get('/', function(req, res){
-    res.send('Data will appear here')
+    res.json()
 })
 
 const PORT = process.env.PORT || 9000;
