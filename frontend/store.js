e1f61959cd1a15bd67f5bce52f2cf2bb381b1a59
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-23 05:24:02 -0700

    Commit Redux Integration

--- frontend/store.js
+++ frontend/store.js
@@ -2,6 +2,7 @@ import {createStore, combineReducers} from 'redux'
 
 const initialState = {}
 const reducer = combineReducers ({
+    productList: productListReducer,
     
 })
 const store = createStore(reducer, initialState)
