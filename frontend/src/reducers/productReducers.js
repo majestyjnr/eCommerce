a8d4445580a2c48263d18007eda072354c82c5fc
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-23 05:56:04 -0700

    Commit Updates

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,13 @@
 
 function productListReducer(state= {products:[]}, action){
     switch(action.type){
-        
+        case PRODUCTS_LIST_REQUEST:
+            return {loading: true};
+        case PRODUCTS_LIST_SUCCESS:
+            return {loading: false, products: action.payload};
+        case PRODUCT_LIST_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
     }
 }
