ccb1c0cd6cd5299c6b1ce646519fa63dfaf6a482
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-23 06:17:04 -0700

    Commit Updates

--- frontend/store.js
+++ frontend/store.js
@@ -7,5 +7,7 @@ const reducer = combineReducers ({
 })      
 
 // ***Thunk*** is a middleware for redux that allows us to async updation inside action in the redux
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)))
-export default store
+export default store    
