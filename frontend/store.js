baf7f62510d9cd2481a0571119e4ce8adbd0ee13
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-23 06:12:14 -0700

    Commit Updates

--- frontend/store.js
+++ frontend/store.js
@@ -1,10 +1,9 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import { productListReducer } from './src/reducers/productReducers'
 
 const initialState = {}
 const reducer = combineReducers ({
     productList: productListReducer,
-
-})
-const store = createStore(reducer, initialState)
+})      // Thunk is a middleware for redux that allows us to async updation inside action in the redux
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)))
 export default store
