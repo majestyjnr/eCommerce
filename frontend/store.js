90092e92e38144e7d8d2eaada827ed1d9990cdf7
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-03-23 06:30:53 -0700

    Commit Updates

--- frontend/store.js
+++ frontend/store.js
@@ -1,4 +1,5 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import { productListReducer } from './src/reducers/productReducers'
 
 const initialState = {}
