2b09134e9a9f6b6983b5a853bf5ec7585fadb11e
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-03 13:49:21 -0700

    Commit Updates

---
+++ models/Order.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema({
+    customerId :{
+        type: String,
+        required: true
+    },
+    customerPhone :{
+        type: String,
+        required: true
+    },
+    products :{
+        type: Array,
+        required: true
+    },
+    isPaid :{
+        type: Boolean,
+        required: true
+    },
+    deliveryAddress :{
+        type: String,
+        required: true
+    }
+}, {timestamps: true}) 
+
+const Order = mongoose.model('order', OrderSchema)
+module.exports = Order
